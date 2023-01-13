93ec978f847e9a54193408ef9c1b746e43596c1b
Romit Gabani <85230081+romitg2@users.noreply.github.com>
2023-01-13 18:59:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -13,19 +13,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const {renderer, scene, camera} = mindarThree;
 
     // creating three.js objects
+    const scale = 3;
     
-    const geometry = new THREE.PlaneGeometry(1, 1);
+    const geometryOne = new THREE.PlaneGeometry(0.1 * scale, 0.1 * scale);
+    const geometryTwo = new THREE.PlaneGeometry(0.1 * scale, 0.1 * scale);
+    const geometryThree = new THREE.PlaneGeometry(0.1 * scale, 0.1 * scale);
     const material = new THREE.MeshBasicMaterial({ color: 0xffff00, transprent: true, opacity: 0.1});
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    mesh.position.set(0, 0, 0);
+    const meshOne = new THREE.Mesh(geometryOne, material);
+    const meshTwo = new THREE.Mesh(geometryTwo, material);
+    const meshThree = new THREE.Mesh(geometryThree, material);
+
+    meshOne.position.set(1, -0.5, 0);
+    meshTwo.position.set(1, 0, 0);
+    meshThree.position.set(1, 0.5, 0);
+    
+    const meshGroup = new THREE.Group();
+    meshGroup.add(meshOne, meshTwo, meshThree);
 
     // creating anchor element
 
     const anchor = mindarThree.addAnchor(0);
     console.log(anchor);
     console.log(anchor.group);
-    anchor.group.add(mesh);
+    anchor.group.add(meshGroup);
     console.log(anchor.group);
 
     // starting the mindar engine
